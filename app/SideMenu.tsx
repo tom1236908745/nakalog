420faa0a6447a5f316022ced38c2b968df1bd624
tom1236908745 <tom1233908745@gmail.com>
2023-01-04 16:13:12 +0900

    add: 書き方変更

--- app/SideMenu.tsx
+++ app/SideMenu.tsx
@@ -11,13 +11,11 @@ export default function SideMenue() {
     <div>
       <HumberguerMenu />
       <div className={`${side.menu} ${center ? side.checkedMenu : side.color}`}>
-        {center ? (
-          <ul className={side.links}>
-            <li className={side.link1}>リンク1</li>
-            <li className={side.link2}>リンク2</li>
-            <li className={side.link3}>リンク3</li>
-          </ul>
-        ) : null}
+        <ul className={`${center ? side.links : ''}`}>
+          <li className={`${center ? side.link1 : ''}`}>リンク1</li>
+          <li className={`${center ? side.link2 : ''}`}>リンク2</li>
+          <li className={`${center ? side.link3 : ''}`}>リンク3</li>
+        </ul>
       </div>
     </div>
   );
