011fd942b37c971416da4212c4613eecd39fa6b3
tom1236908745 <tom1233908745@gmail.com>
2023-01-05 23:23:00 +0900

    add: 外側のクリック制御

--- app/SideMenu.tsx
+++ app/SideMenu.tsx
@@ -1,16 +1,45 @@
 'use client';
-import { useState } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useState, useEffect, useRef } from 'react';
+import { useRecoilState } from 'recoil';
 import { checkedState } from '../atoms/CheckedAtom.js';
 import side from '../styles/sideMenu.module.css';
 import HumberguerMenu from './HumbergerMenu';
 
 export default function SideMenue() {
-  const checked = useRecoilValue(checkedState);
+  const [checked, setChecked] = useRecoilState(checkedState);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const sideRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    //対象の要素を取得
+    const menuEl = menuRef.current;
+    const sideEl = sideRef.current;
+
+    //対象の要素がなければ何もしない
+    if (!menuEl && !sideEl) return;
+
+    //クリックした時に実行する関数
+    const hundleClickOutside = (e: MouseEvent) => {
+      // サイドメニューをクリックした時
+      if (menuEl?.contains(e.target as Node) && checked) {
+        return;
+      }
+      setChecked(false);
+    };
+
+    document.addEventListener('click', hundleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', hundleClickOutside);
+    };
+  }, [checked]);
   return (
-    <div>
+    <div ref={sideRef}>
       <HumberguerMenu />
-      <nav className={`${side.menu} ${checked ? side.checkedMenu : ''}`}>
+      <nav
+        className={`${side.menu} ${checked ? side.checkedMenu : ''}`}
+        ref={menuRef}
+      >
         <ul className={side.links}>
           <li
             className={`${side.link1} ${
