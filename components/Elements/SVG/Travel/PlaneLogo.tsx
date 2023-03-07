1418dd5a3048574d2032e8e29264d62dea94e9a6
tom1236908745 <tom1233908745@gmail.com>
2023-03-07 18:23:48 +0900

    feat: 一覧ページの大まかなレイアウト

--- components/Elements/SVG/Travel/PlaneLogo.tsx
+++ components/Elements/SVG/Travel/PlaneLogo.tsx
@@ -1,11 +1,17 @@
 'use client';
-import React, { useState } from 'react';
-import ReactFreezeframe from 'react-freezeframe';
+import React from 'react';
+import categoryLogoStyle from '@/styles/article/categoryLogoStyle.module.css';
 export const PlaneLogo = () => {
   return (
-    <div>
-      {/* <ReactFreezeframe src="./images/travel/plane.gif"></ReactFreezeframe> */}
-      <img style={{ height: '85vh' }} src="./images/travel/plane.gif" />
+    <div className={categoryLogoStyle.categoryLogoWrapperStyle}>
+      <div className={categoryLogoStyle.categoryTitleWrapperStyle}>
+        <div className={categoryLogoStyle.categoryTitleEnStyle}>Visit</div>
+        <div className={categoryLogoStyle.categoryTitleJpStyle}>旅行</div>
+      </div>
+      <img
+        src="./images/travel/plane.gif"
+        className={categoryLogoStyle.categoryImageStyle}
+      />
     </div>
   );
 };
